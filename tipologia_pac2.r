2d4a1ff0151c9e8531c99d3ddd0f0ae1f0f9d9cd
Alejandra-Cristina <amarrerosuarez@uoc.edu>
2021-05-23 09:52:04 +0100

    Archivo R con código de la práctica

---
+++ tipologia_pac2.r
@@ -0,0 +1,22 @@
+doc_csv <-read.csv("INSURANCE.csv",header=TRUE)
+head(doc_csv)
+str(doc_csv)
+table(doc_csv$sex)
+table(doc_csv$smoker)
+
+res <- sapply(doc_csv,class)
+res <- which(res=="numeric")
+
+par(mfrow=c(1,2))
+for(i in 1:2){
+	boxplot(doc_csv[,res[i]],main=names(doc_csv)[res[i]],col="gray")
+}
+
+> clus <- doc_csv[,c("age","sex","bmi","children","smoker","region","charges")]
+> clus
+
+mean.n <- as.vector(sapply( doc_csv[,res ],mean,na.rm=TRUE ) )
+std.n <- as.vector(sapply(doc_csv[,res ],sd, na.rm=TRUE))
+median.n <- as.vector(sapply(doc_csv[,res],median, na.rm=TRUE))
+
+
